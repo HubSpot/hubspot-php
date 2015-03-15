77658c45ab8abed58c5e7bc02c682b1f0634019a
fungku <fungku@gmail.com>
2015-03-14 17:09:37 -0700

    Workflows

--- src/Api/Workflows.php
+++ src/Api/Workflows.php
@@ -3,43 +3,112 @@
 class Workflows extends Api
 {
     /**
-     * Get all blogs.
+     * Get all workflows.
      *
-     * @param array $params Optional parameters ['limit', 'offset', 'created', 'deleted_at', 'name']
      * @return mixed
      */
-    public function all(array $params = [])
+    public function all()
     {
-        $endpoint = '/content/api/v2/blogs';
-
-        $options['query'] = $params;
-
-        return $this->request('get', $endpoint, $options);
-    }
-
-    /**
-     * Get information about a specific blog.
-     *
-     * @param string $id
-     * @return mixed
-     */
-    public function getById($id)
-    {
-        $endpoint = "/content/api/v2/blogs/{$id}";
+        $endpoint = "/automation/v2/workflows";
 
         return $this->request('get', $endpoint);
     }
 
     /**
-     * Get previous versions of the blog.
+     * Get a specific workflow.
      *
-     * @param string $id     Blog id.
-     * @param array  $params Optional parameters.
+     * @param int $id
      * @return mixed
      */
-    public function versions($id, array $params = [])
+    public function getById($id)
     {
-        $endpoint = "/content/api/v2/blogs/{$id}/versions";
+        $endpoint = "/automation/v2/workflows/{$id}";
+
+        return $this->request('get', $endpoint);
+    }
+
+    /**
+     * Enroll a contact in a workflow.
+     *
+     * @param int $workflow_id
+     * @param string $email
+     * @return mixed
+     */
+    public function enrollContact($workflow_id, $email)
+    {
+        $endpoint = "/automation/v2/workflows/{$workflow_id}/enrollments/contacts/{$email}";
+
+        return $this->request('get', $endpoint);
+    }
+
+    /**
+     * Unenroll a contact from a workflow.
+     *
+     * @param int $workflow_id
+     * @param string $email
+     * @return mixed
+     */
+    public function unenrollContact($workflow_id, $email)
+    {
+        $endpoint = "/automation/v2/workflows/{$workflow_id}/enrollments/contacts/{$email}";
+
+        return $this->request('delete', $endpoint);
+    }
+
+    /**
+     * Create a new workflow.
+     *
+     * @param array $workflow The workflow properties
+     * @return mixed
+     */
+    public function create(array $workflow)
+    {
+        $endpoint = "/automation/v2/workflows";
+
+        $options['json'] = $workflow;
+
+        return $this->request('post', $endpoint, $options);
+    }
+
+    /**
+     * Delete a workflow.
+     *
+     * @param int $id
+     * @return mixed
+     */
+    public function delete($id)
+    {
+        $endpoint = "/automation/v2/workflows/{$id}";
+
+        $options['query'] = ['updatedAt' => time()];
+
+        return $this->request('delete', $endpoint, $options);
+    }
+
+    /**
+     * Get current enrollments for a contact.
+     *
+     * @param int $contact_id
+     * @return mixed
+     */
+    public function enrollmentsForContact($contact_id)
+    {
+        $endpoint = "/automation/v2/workflows/enrollments/contacts/{$contact_id}";
+
+        return $this->request('get', $endpoint);
+    }
+
+    /**
+     * Get past events for contact from a workflow.
+     *
+     * @param int $workflow_id
+     * @param int $contact_id
+     * @param array $params Optional parameters.
+     * @return mixed
+     */
+    public function pastEventsForContact($workflow_id, $contact_id, array $params = [])
+    {
+        $endpoint = " /automation/v2/workflows/{$workflow_id}/logevents/contacts/{$contact_id}/past";
 
         $options['query'] = $params;
 
@@ -47,19 +116,20 @@ class Workflows extends Api
     }
 
     /**
-     * Get a previous version of the blog.
+     * Get upcoming (scheduled) events for a contact in a workflow.
      *
-     * @param string $id         Blog id.
-     * @param string $version_id Version id.
-     * @param array  $params     Optional parameters.
+     * @param int $workflow_id
+     * @param int $contact_id
+     * @param array $params
      * @return mixed
      */
-    public function getVersionById($id, $version_id, array $params = [])
+    public function upcomingEventsForContact($workflow_id, $contact_id, array $params = [])
     {
-        $endpoint = "/content/api/v2/blogs/{$id}/versions/{$version_id}";
+        $endpoint = "/automation/v2/workflows/{$workflow_id}/logevents/contacts/{$contact_id}/upcoming";
 
         $options['query'] = $params;
 
         return $this->request('get', $endpoint, $options);
     }
+
 }
