140022d9295c93b45c5996c056d7617c96114280
fungku <fungku@gmail.com>
2015-04-18 18:39:03 -0700

    Update contact batch endpoints to fix property param

--- src/Api/Contacts.php
+++ src/Api/Contacts.php
@@ -74,7 +74,18 @@ class Contacts extends Api
     }
 
     /**
-     * @param array $params Optional parameters ['count', 'property', 'vidOffset']
+     * For a given portal, return all contacts that have been created in the portal.
+     *
+     * A paginated list of contacts will be returned to you, with a maximum of 100 contacts per page.
+     *
+     * Please Note: There are 2 fields here to pay close attention to: the "has-more" field that will let you know
+     * whether there are more contacts that you can pull from this portal, and the "vid-offset" field which will let
+     * you know where you are in the list of contacts. You can then use the "vid-offset" field in the "vidOffset"
+     * parameter described below.
+     *
+     * @link http://developers.hubspot.com/docs/methods/contacts/get_contacts
+     *
+     * @param array $params Array of optional parameters ['count', 'property', 'vidOffset']
      * @return mixed
      */
     public function all($params)
@@ -98,10 +109,9 @@ class Contacts extends Api
      * A paginated list of contacts will be returned to you, with a maximum of 100 contacts per page, as specified by
      * the "count" parameter. The endpoint only scrolls back in time 30 days.
      *
-     * @param mixed[] $params Array of optional parameters ['count', 'timeOffset', 'vidOffset', 'property', 'propertyMode', 'formSubmissionMode', 'showListMemberships']
-     *
      * @link http://developers.hubspot.com/docs/methods/contacts/get_recently_updated_contacts
      *
+     * @param array $params Array of optional parameters ['count', 'timeOffset', 'vidOffset', 'property', 'propertyMode', 'formSubmissionMode', 'showListMemberships']
      * @return mixed
      */
     public function recent($params)
@@ -132,8 +142,16 @@ class Contacts extends Api
     }
 
     /**
-     * @param array $vids
-     * @param array $params
+     * For a given portal, return information about a group of contacts by their unique ID's. A contact's unique ID's
+     * is stored in a field called 'vid' which stands for 'visitor ID'.
+     *
+     * This method will also return you much of the HubSpot lead "intelligence" for each requested contact record. The
+     * endpoint accepts many query parameters that allow for customization based on a variety of integration use cases.
+     *
+     * @link http://developers.hubspot.com/docs/methods/contacts/get_batch_by_vid
+     *
+     * @param array $vids Array of visitor IDs
+     * @param array $params Array of optional parameters ['property', 'propertyMode', 'formSubmissionMode', 'showListMemberships', 'includeDeletes']
      * @return mixed
      */
     public function getBatchByIds(array $vids, $params)
@@ -141,6 +159,11 @@ class Contacts extends Api
         $endpoint = "/contacts/v1/contact/vids/batch/";
 
         $queryString = $this->generateBatchQuery('vid', $vids);
+
+        if (isset($params['property']) && is_array($params['property'])) {
+            $queryString .= $this->generateBatchQuery('property', $params['property']);
+            unset($params['property']);
+        }
 
         $options['query'] = $this->getQuery($params);
 
@@ -159,8 +182,15 @@ class Contacts extends Api
     }
 
     /**
-     * @param array $emails
-     * @param array $params
+     * For a given portal, return information about a group of contacts by their email addresses.
+     *
+     * This method will also return you much of the HubSpot lead "intelligence" for each requested contact record. The
+     * endpoint accepts many query parameters that allow for customization based on a variety of integration use cases.
+     *
+     * @link http://developers.hubspot.com/docs/methods/contacts/get_batch_by_email
+     *
+     * @param array $emails Array of email adresses
+     * @param array $params Array of optional parameters ['property', 'propertyMode', 'formSubmissionMode', 'showListMemberships', 'includeDeletes']
      * @return mixed
      */
     public function getBatchByEmails(array $emails, $params)
@@ -168,6 +198,11 @@ class Contacts extends Api
         $endpoint = "/contacts/v1/contact/vids/batch/";
 
         $queryString = $this->generateBatchQuery('email', $emails);
+
+        if (isset($params['property']) && is_array($params['property'])) {
+            $queryString .= $this->generateBatchQuery('property', $params['property']);
+            unset($params['property']);
+        }
 
         $options['query'] = $this->getQuery($params);
 
@@ -186,8 +221,19 @@ class Contacts extends Api
     }
 
     /**
-     * @param array $utks
-     * @param array $params
+     * For a given portal, return information about a group of contacts by their user tokens (hubspotutk).
+     *
+     * This method will also return you much of the HubSpot lead "intelligence" for each requested contact
+     * record. The endpoint accepts many query parameters that allow for customization based on a variety of
+     * integration use cases.
+     *
+     * The endpoint does not allow for CORS, so if you are looking up contacts from their user token on the client,
+     * you'll need to spin up a proxy server to interact with the API.
+     *
+     * @link http://developers.hubspot.com/docs/methods/contacts/get_batch_by_utk
+     *
+     * @param array $utks Array of hubspot user tokens (hubspotutk)
+     * @param array $params Array of optional parameters ['property', 'propertyMode', 'formSubmissionMode', 'showListMemberships', 'includeDeletes']
      * @return mixed
      */
     public function getBatchByTokens(array $utks, $params)
@@ -196,14 +242,29 @@ class Contacts extends Api
 
         $queryString = $this->generateBatchQuery('utk', $utks);
 
+        if (isset($params['property']) && is_array($params['property'])) {
+            $queryString .= $this->generateBatchQuery('property', $params['property']);
+            unset($params['property']);
+        }
+
         $options['query'] = $this->getQuery($params);
 
         return $this->request('get', $endpoint, $options, $queryString);
     }
 
     /**
-     * @param string $query
-     * @param array  $params
+     * For a given portal, return contacts and some data associated with
+     * those contacts by the contact's email address or name.
+     *
+     * Please note that you should expect this method to only return a small
+     * subset of data about the contact. One piece of data that the method will
+     * return is the contact ID (vid) that you can then use to look up much
+     * more data about that particular contact by its ID.
+     *
+     * @link http://developers.hubspot.com/docs/methods/contacts/search_contacts
+     *
+     * @param string $query Search query
+     * @param array $params Array of optional parameters ['count', 'offset']
      * @return mixed
      */
     public function search($query, $params)
