7627b32faef4298acbff771583902ec33c33a7b4
ryanwinchester <fungku@gmail.com>
2015-10-19 00:02:35 -0700

    Remove spec test helper

--- tests/spec/Api/BlogPostsSpec.php
+++ tests/spec/Api/BlogPostsSpec.php
@@ -5,12 +5,9 @@ namespace spec\Fungku\HubSpot\Api;
 use Fungku\HubSpot\Http\Client;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
-use tests\helpers\Fungku\HubSpot\SendsRequests;
 
 class BlogPostsSpec extends ObjectBehavior
 {
-    use SendsRequests;
-
     function let(Client $client)
     {
         $this->client = $client;
@@ -20,24 +17,5 @@ class BlogPostsSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('Fungku\HubSpot\Api\BlogPosts');
-    }
-
-    function it_creates_a_blog_post()
-    {
-        $post = [
-            'name'             => 'My Super Awesome Post ' . uniqid(),
-            'content_group_id' => 351076997,
-        ];
-
-        $url = $this->buildUrl('/content/api/v2/blog-posts');
-
-        $options = [
-            'json' => $post,
-            'headers' => $this->headers
-        ];
-
-        $this->client->post($url, $options)->shouldBeCalled();
-
-        $this->create($post);
     }
 }
--- tests/spec/Api/EventsSpec.php
+++ tests/spec/Api/EventsSpec.php
@@ -5,12 +5,9 @@ namespace spec\Fungku\HubSpot\Api;
 use Fungku\HubSpot\Http\Client;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
-use tests\helpers\Fungku\HubSpot\SendsRequests;
 
 class EventsSpec extends ObjectBehavior
 {
-    use SendsRequests;
-
     function let(Client $client)
     {
         $this->client = $client;
@@ -20,45 +17,5 @@ class EventsSpec extends ObjectBehavior
     function it_is_initializable()
     {
         $this->shouldHaveType('Fungku\HubSpot\Api\Events');
-    }
-
-    function it_triggers()
-    {
-        $this->client->get(
-            'http://track.hubspot.com/v1/event?_a=56043&_n=917',
-            ['headers' => $this->headers]
-        )->shouldBeCalled();
-
-        $this->trigger(56043, 917);
-    }
-
-    function it_triggers_with_email()
-    {
-        $this->client->get(
-            'http://track.hubspot.com/v1/event?_a=56043&_n=917&email=test%40hubspot.com',
-            ['headers' => $this->headers]
-        )->shouldBeCalled();
-
-        $this->trigger(56043, 917, 'test@hubspot.com');
-    }
-
-    function it_triggers_with_revenue()
-    {
-        $this->client->get(
-            'http://track.hubspot.com/v1/event?_a=56043&_n=917&email=test%40hubspot.com&_m=50',
-            ['headers' => $this->headers]
-        )->shouldBeCalled();
-
-        $this->trigger(56043, 917, 'test@hubspot.com', 50);
-    }
-
-    function it_triggers_with_properties()
-    {
-        $this->client->get(
-            'http://track.hubspot.com/v1/event?_a=56043&_n=917&firstname=joe&email=test%40hubspot.com&_m=50',
-            ['headers' => $this->headers]
-        )->shouldBeCalled();
-
-        $this->trigger(56043, 917, 'test@hubspot.com', 50, ['firstname' => 'joe']);
     }
 }
