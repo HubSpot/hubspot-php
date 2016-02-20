7a69a567f267258009b0a63a6219302bcebe6abb
Kevin <kk@mobagel.com>
2016-02-20 16:07:09 +0800

    [FEATURE] add engagements api service

---
+++ src/Api/Engagements.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Fungku\HubSpot\Api;
+
+
+class Engagements extends Api
+{
+    /**
+     * @param array $engagement Array of engagement engagement.
+     * @param array $associations Array of engagement associations.
+     * @param array $metadata Array of engagement metadata.
+     * @return \Fungku\HubSpot\Http\Response
+     */
+    public function create($engagement, $associations, $metadata)
+    {
+        $endpoint = "/engagements/v1/engagements";
+
+        $options['json'] = [
+            'engagement' => $engagement,
+            'associations' => $associations,
+            'metadata' => $metadata,
+        ];
+
+        return $this->request('post', $endpoint, $options);
+    }
+
+    /**
+     * @param int   $id         The engagement id.
+     * @param array $engagement The engagement engagement to update.
+     * @param array $metadata The engagement metadata to update.
+     * @return \Fungku\HubSpot\Http\Response
+     */
+    public function update($id, $engagement, $metadata)
+    {
+        $endpoint = "/engagements/v1/engagements/{$id}";
+
+        $options['json'] = [
+            'engagement' => $engagement,
+            'metadata' => $metadata,
+        ];
+
+        return $this->request('put', $endpoint, $options);
+    }
+
+    /**
+     * @param int $id
+     * @return \Fungku\HubSpot\Http\Response
+     */
+    public function delete($id)
+    {
+        $endpoint = "/engagements/v1/engagements/{$id}";
+
+        return $this->request('delete', $endpoint);
+    }
+
+    /**
+     * @param int $id
+     * @return \Fungku\HubSpot\Http\Response
+     */
+    public function get($id)
+    {
+        $endpoint = "/engagements/v1/engagements/{$id}";
+
+        return $this->request('get', $endpoint);
+    }
+
+    /**
+     * @param int $id
+     * @param string $object_type
+     * @param int $object_id
+     * @return \Fungku\HubSpot\Http\Response
+     **/
+    public function associate($id, $object_type, $object_id)
+    {
+        $endpoint = "/engagements/v1/engagements/{$id}/associations/{$object_type}/{$object_id}";
+
+        return $this->request('put', $endpoint);
+
+    }
+
+}
---
+++ tests/integration/Api/EngagementsTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Fungku\HubSpot\Tests\Integration\Api;
+
+use Fungku\HubSpot\Api\Engagements;
+use Fungku\HubSpot\Api\Contacts;
+use Fungku\HubSpot\Http\Client;
+
+class EngagementsTest extends \PHPUnit_Framework_TestCase
+{
+    private $engagements;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->contacts = new Contacts('demo', new Client());
+        $this->engagements = new Engagements('demo', new Client());
+        sleep(1);
+    }
+
+    /*
+     * Lots of tests need an existing object to modify.
+     */
+    private function createEngagement()
+    {
+        sleep(1);
+        $contact = $this->contacts->create([]);
+        $response = $this->engagements->create([
+            "active" => true,
+            "ownerId" => 1,
+            "type" => "NOTE",
+            "timestamp" => 1409172644778,
+        ],[
+            "contactIds" => [$contact->vid],
+            "companyIds" => [],
+            "dealIds" => [],
+            "ownerIds" => [],
+        ],[
+            'body' => 'note body',
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        return $response;
+    }
+
+    /** @test */
+    public function update()
+    {
+        $engagement = $this->createEngagement();
+        $contact = $this->contacts->create([]);
+        $response = $this->engagements->update($engagement->engagement->id, [
+            "active" => true,
+            "ownerId" => 1,
+            "type" => "NOTE",
+            "timestamp" => 1409172644778
+        ],[
+            'body' => 'note body',
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    /** @test */
+    public function delete()
+    {
+        $engagement = $this->createEngagement();
+
+        $response = $this->engagements->delete($engagement->engagement->id);
+
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+
+    /** @test */
+    public function get()
+    {
+        $engagement = $this->createEngagement();
+
+        $response = $this->engagements->get($engagement->engagement->id);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    /** @test */
+    public function associate()
+    {
+        $engagement = $this->createEngagement();
+        $contact = $this->contacts->create([]);
+        $response = $this->engagements->associate($engagement->engagement->id, 'contact', $contact->vid);
+
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+
+
+}
