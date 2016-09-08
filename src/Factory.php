42fb03684968936f16d6e6ab6ba0f45240c82975
John Congdon <john@johncongdon.com>
2016-09-08 16:06:58 -0700

    Added missing resources as a factory method for IDE awareness (#57)
    * Added the engagements resource as a factory method for IDE awareness
    
    * Added missing resources

--- src/Factory.php
+++ src/Factory.php
@@ -7,6 +7,7 @@ use SevenShores\Hubspot\Http\Client;
 /**
  * Class Factory
  *
+ * @method \SevenShores\Hubspot\Resources\BlogAuthors blogauthors()
  * @method \SevenShores\Hubspot\Resources\Blogs blogs()
  * @method \SevenShores\Hubspot\Resources\BlogPosts blogPosts()
  * @method \SevenShores\Hubspot\Resources\BlogTopics blogTopics()
@@ -17,14 +18,15 @@ use SevenShores\Hubspot\Http\Client;
  * @method \SevenShores\Hubspot\Resources\Contacts contacts()
  * @method \SevenShores\Hubspot\Resources\Email email()
  * @method \SevenShores\Hubspot\Resources\EmailEvents emailEvents()
+ * @method \SevenShores\Hubspot\Resources\Engagements engagements()
  * @method \SevenShores\Hubspot\Resources\Files files()
  * @method \SevenShores\Hubspot\Resources\Forms forms()
  * @method \SevenShores\Hubspot\Resources\Keywords keywords()
- * @method \SevenShores\Hubspot\Resources\MarketPlace marketPlace()
  * @method \SevenShores\Hubspot\Resources\Pages pages()
  * @method \SevenShores\Hubspot\Resources\SocialMedia socialMedia()
  * @method \SevenShores\Hubspot\Resources\Workflows workflows()
  * @method \SevenShores\Hubspot\Resources\Events events()
+ * @method \SevenShores\Hubspot\Resources\DealProperties dealproperties()
  * @method \SevenShores\Hubspot\Resources\Deals deals()
  * @method \SevenShores\Hubspot\Resources\Owners owners()
  */
