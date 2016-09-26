5e2075f01255285da5f419e76348c80e64ea8121
Chris Class <chris@trendwatching.com>
2016-09-26 13:11:15 +0100

    adds support for Single Send API

---
+++ src/Resources/SingleEmail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SevenShores\Hubspot\Resources;
+
+class SingleEmail extends Resource
+{
+    /**
+     * Send an email designed and maintained in the HubSpot marketing Email Tool.
+     *
+     * @param array  $params
+     * @return \SevenShores\Hubspot\Http\Response
+     */
+    function send($id, $message = [], $contactProperties = [], $customProperties = [])
+    {
+        $endpoint = "https://api.hubapi.com/email/public/v1/singleEmail/send";
+
+        $options['json'] = [
+          'emailId'           => $id,
+          'message'           => $message
+          'contactProperties' => $contactProperties,
+          'customProperties'  => $customProperties
+        ];
+
+        return $this->client->request('post', $endpoint, $options);
+    }
+}
