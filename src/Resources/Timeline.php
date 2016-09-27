8acaaa1ddb0b6ac26ece9d3f1351d29376141db7
Luke Evers <luke@lukevers.com>
2016-09-27 17:59:37 -0400

    Create a Timeline class with empty functions to implement.

---
+++ src/Resources/Timeline.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace SevenShores\Hubspot\Resources;
+
+class Timeline extends Resource
+{
+    /**
+     * @see http://developers.hubspot.com/docs/methods/timeline/create-or-update-event
+     */
+    public function createOrUpdate()
+    {
+        //
+    }
+
+    /**
+     * @see http://developers.hubspot.com/docs/methods/timeline/get-event-types
+     */
+    public function getEventTypes()
+    {
+        //
+    }
+
+    /**
+     * @see http://developers.hubspot.com/docs/methods/timeline/create-event-type
+     */
+    public function createEventType()
+    {
+        //
+    }
+
+    /**
+     * @see http://developers.hubspot.com/docs/methods/timeline/update-event-type
+     */
+    public function updateEventType()
+    {
+        //
+    }
+
+    /**
+     * @see http://developers.hubspot.com/docs/methods/timeline/delete-event-type
+     */
+    public function deleteEventType()
+    {
+        //
+    }
+
+    /**
+     * @see http://developers.hubspot.com/docs/methods/timeline/get-timeline-event-type-properties
+     */
+    public function getEventTypeProperties()
+    {
+        //
+    }
+
+    /**
+     * @see http://developers.hubspot.com/docs/methods/timeline/create-timeline-event-type-property
+     */
+    public function createEventTypeProperty()
+    {
+        //
+    }
+
+    /**
+     * @see http://developers.hubspot.com/docs/methods/timeline/delete-timeline-event-type-property
+     */
+    public function updateEventTypeProperty()
+    {
+        //
+    }
+
+    /**
+     * @see http://developers.hubspot.com/docs/methods/timeline/delete-timeline-event-type-property
+     */
+    public function deleteEventTypeProperty()
+    {
+        //
+    }
+}
