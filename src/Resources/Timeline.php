6db469223f2e9725c1d3bcb912009dbc46718e5c
Luke Evers <luke@lukevers.com>
2016-09-27 18:19:44 -0400

    Add an endpoint for every Timeline function

--- src/Resources/Timeline.php
+++ src/Resources/Timeline.php
@@ -5,74 +5,98 @@ namespace SevenShores\Hubspot\Resources;
 class Timeline extends Resource
 {
     /**
+     * @param string $appId
+     *
      * @see http://developers.hubspot.com/docs/methods/timeline/create-or-update-event
      */
-    public function createOrUpdate()
+    public function createOrUpdate($appId)
     {
-        //
+        $endpoint = "https://api.hubapi.com/integrations/v1/{$appId}/timeline/event";
     }
 
     /**
+     * @param string $appId
+     *
      * @see http://developers.hubspot.com/docs/methods/timeline/get-event-types
      */
-    public function getEventTypes()
+    public function getEventTypes($appId)
     {
-        //
+        $endpoint = "https://api.hubapi.com/integrations/v1/{$appId}/timeline/event-types";
     }
 
     /**
+     * @param string $appId
+     *
      * @see http://developers.hubspot.com/docs/methods/timeline/create-event-type
      */
-    public function createEventType()
+    public function createEventType($appId)
     {
-        //
+        $endpoint = "https://api.hubapi.com/integrations/v1/{$appId}/timeline/event-types";
     }
 
     /**
+     * @param string $appId
+     * @param string $eventTypeId
+     *
      * @see http://developers.hubspot.com/docs/methods/timeline/update-event-type
      */
-    public function updateEventType()
+    public function updateEventType($appId, $eventTypeId)
     {
-        //
+        $endpoint = "https://api.hubapi.com/integrations/v1/{$appId}/timeline/event-types/{$eventTypeId}";
     }
 
     /**
+     * @param string $appId
+     * @param string $eventTypeId
+     *
      * @see http://developers.hubspot.com/docs/methods/timeline/delete-event-type
      */
-    public function deleteEventType()
+    public function deleteEventType($appId, $eventTypeId)
     {
-        //
+        $endpoint = "https://api.hubapi.com/integrations/v1/{$appId}/timeline/event-types/{$eventTypeId}";
     }
 
     /**
+     * @param string $appId
+     * @param string $eventTypeId
+     *
      * @see http://developers.hubspot.com/docs/methods/timeline/get-timeline-event-type-properties
      */
-    public function getEventTypeProperties()
+    public function getEventTypeProperties($appId, $eventTypeId)
     {
-        //
+        $endpoint = "https://api.hubapi.com/integrations/v1/{$appId}/timeline/event-types/{$eventTypeId}/properties";
     }
 
     /**
+     * @param string $appId
+     * @param string $eventTypeId
+     *
      * @see http://developers.hubspot.com/docs/methods/timeline/create-timeline-event-type-property
      */
-    public function createEventTypeProperty()
+    public function createEventTypeProperty($appId, $eventTypeId)
     {
-        //
+        $endpoint = "https://api.hubapi.com/integrations/v1/{$appId}/timeline/event-types/{$eventTypeId}/properties";
     }
 
     /**
+     * @param string $appId
+     * @param string $eventTypeId
+     *
      * @see http://developers.hubspot.com/docs/methods/timeline/delete-timeline-event-type-property
      */
-    public function updateEventTypeProperty()
+    public function updateEventTypeProperty($appId, $eventTypeId)
     {
-        //
+        $endpoint = "https://api.hubapi.com/integrations/v1/{$appId}/timeline/event-types/{$eventTypeId}/properties";
     }
 
     /**
+     * @param string $appId
+     * @param string $eventTypeId
+     *
      * @see http://developers.hubspot.com/docs/methods/timeline/delete-timeline-event-type-property
      */
-    public function deleteEventTypeProperty()
+    public function deleteEventTypeProperty($appId, $eventTypeId)
     {
-        //
+        $endpoint = "https://api.hubapi.com/integrations/v1/{$appId}/timeline/event-types/{$eventTypeId}/properties";
     }
 }
