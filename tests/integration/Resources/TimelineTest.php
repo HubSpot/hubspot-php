bc5639f18ec0b6bbf3a169d15528e7f479839c08
Luke Evers <luke@lukevers.com>
2016-09-27 18:09:17 -0400

    Add empty tests for every function in Timeline

--- tests/integration/Resources/TimelineTest.php
+++ tests/integration/Resources/TimelineTest.php
@@ -20,4 +20,76 @@ class TimelineTest extends \PHPUint_Framework_TestCase
         $this->timeline = new Timeline(new Client(['key' => 'demo']));
         sleep(1);
     }
+
+    /**
+     * @test
+     */
+    public function createOrUpdate()
+    {
+        // @todo
+    }
+
+    /**
+     * @test
+     */
+    public function getEventTypes()
+    {
+        // @todo
+    }
+
+    /**
+     * @test
+     */
+    public function createEventType()
+    {
+        // @todo
+    }
+
+    /**
+     * @test
+     */
+    public function updateEventType()
+    {
+        // @todo
+    }
+
+    /**
+     * @test
+     */
+    public function deleteEventType()
+    {
+        // @todo
+    }
+
+    /**
+     * @test
+     */
+    public function getEventTypeProperties()
+    {
+        // @todo
+    }
+
+    /**
+     * @test
+     */
+    public function createEventTypeProperty()
+    {
+        // @todo
+    }
+
+    /**
+     * @test
+     */
+    public function updateEventTypeProperty()
+    {
+        // @todo
+    }
+
+    /**
+     * @test
+     */
+    public function deleteEventTypeProperty()
+    {
+        // @todo
+    }
 }
