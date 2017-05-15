f620bac70bde7143d55823ad93ddf6aebc36dff4
Dave Pagini <davidpagini@gmail.com>
2017-05-15 18:11:33 -0400

    Correctly call Event API.

--- src/Resources/Events.php
+++ src/Resources/Events.php
@@ -13,7 +13,7 @@ class Events extends Resource
      *                                     footer of the HubSpot UI, or in the URL. For example, in this URL:
      *                                     "https://app.hubspot.com/reports/56043/events/" your Hub ID is "56043".
      * @param  string $eventId
-     * @param  string $contactEmail        Optional - contact email.
+     * @param  string $contactEmail        Contact email triggering the event.
      * @param  float  $contactRevenue      Optional - the monetary value this event means to you.
      * @param  array  $contactProperties   Optional - array of new contact properties.
      * @return \SevenShores\Hubspot\Http\Response
@@ -25,16 +25,19 @@ class Events extends Resource
         $contactRevenue = null,
         $contactProperties = []
     ) {
-        $endpoint = sprintf(
-            "http://track.hubspot.com/v1/event?_a=%s&_n=%s",
-            url_encode($hubId),
-            url_encode($eventId)
+        $endpoint = "http://track.hubspot.com/v1/event";
+
+        $required['_a'] = $hubId;
+        $required['_n'] = $eventId;
+        $required['email'] = $contactEmail;
+
+        $parameters = array_merge(
+          $required,
+          ['_m' => $contactRevenue],
+          $contactProperties
         );
 
-        $contactProperties['email'] = $contactEmail;
-        $contactProperties['_m'] = $contactRevenue;
-
-        $query_string = build_query_string($contactProperties);
+        $query_string = build_query_string($parameters);
 
         return $this->client->request('get', $endpoint, [], $query_string);
     }
