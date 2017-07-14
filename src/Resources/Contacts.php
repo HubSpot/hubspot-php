ea776b9bb4047be0acd32b2738648d85e7ad6c7e
Wilson Pinto <wilson@virtuagym.com>
2017-07-14 12:42:57 +0200

    Implement optional params on every possible call

--- src/Resources/Contacts.php
+++ src/Resources/Contacts.php
@@ -130,13 +130,17 @@ class Contacts extends Resource
 
     /**
      * @param int $id
+     * @param array $params Array of optional parameters ['property', 'propertyMode', 'formSubmissionMode',
+     *                      'showListMemberships']
      * @return \SevenShores\Hubspot\Http\Response
      */
-    function getById($id)
+    function getById($id, $params = [])
     {
         $endpoint = "https://api.hubapi.com/contacts/v1/contact/vid/{$id}/profile";
 
-        return $this->client->request('get', $endpoint);
+        $queryString = build_query_string($params);
+
+        return $this->client->request('get', $endpoint, [], $queryString);
     }
 
     /**
@@ -205,13 +209,17 @@ class Contacts extends Resource
 
     /**
      * @param string $utk
+     * @param array $params Array of optional parameters ['property', 'propertyMode', 'formSubmissionMode',
+     *                      'showListMemberships']
      * @return \SevenShores\Hubspot\Http\Response
      */
-    function getByToken($utk)
+    function getByToken($utk, $params = [])
     {
         $endpoint = "https://api.hubapi.com/contacts/v1/contact/utk/{$utk}/profile";
 
-        return $this->client->request('get', $endpoint);
+        $queryString = build_query_string($params);
+
+        return $this->client->request('get', $endpoint, [], $queryString);
     }
 
     /**
