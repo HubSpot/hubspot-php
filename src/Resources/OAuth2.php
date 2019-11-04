d4d5dd16995243b46afa57214f501fdc969c3297
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-11-04 16:57:38 +0300

    Clean up

--- src/Resources/OAuth2.php
+++ src/Resources/OAuth2.php
@@ -2,6 +2,8 @@
 
 namespace SevenShores\Hubspot\Resources;
 
+use SevenShores\Hubspot\Exceptions\BadRequest;
+use SevenShores\Hubspot\Http\Response;
 use SevenShores\Hubspot\Utils;
 
 class OAuth2 extends Resource
@@ -11,15 +13,33 @@ class OAuth2 extends Resource
     /**
      * Initiate an Integration with OAuth 2.0
      *
-     * @param string $clientId The Client ID of your app.
-     * @param string $redirectURI The URL that you want the visitor redirected to after granting access to your app. For security reasons, this URL must use https.
-     * @param array $scopesArray A set of scopes that your app will need access to.
-     * @param array $optionalScopesArray A set of optional scopes that your app will need access to.
+     * @param string $clientId      The Client ID of your app.
+     * @param string $redirectURI   The URL that you want the visitor redirected to after granting access to your app. For security reasons, this URL must use https.
+     * @param array  $scopes  A set of scopes that your app will need access to.
+     * @param array  $optionalScopes   A set of optional scopes that your app will need access to.
      * @return string
      */
-    function getAuthUrl($clientId, $redirectURI, $scopesArray = array(), $optionalScopesArray = array())
+    public function getAuthUrl($clientId, $redirectURI, $scopes = [], $optionalScopes = [])
     {
-        return Utils::getFactory()->oAuth2()->getAuthUrl($clientId, $redirectURI, $scopesArray, $optionalScopesArray);
+        return Utils::getFactory()->oAuth2()->getAuthUrl($clientId, $redirectURI, $scopes, $optionalScopes);
+    }
+
+    /**
+     *
+     * @param array $scopes
+     * @return string
+     */
+    protected function scopeToString(array $scopes)
+    {
+        $string = '';
+        foreach($scopes as $scope) {
+            if(!empty($string))
+                $string .= "%20";
+
+            $string .= $scope;
+        }
+
+        return $string;
     }
 
     /**
@@ -29,10 +49,10 @@ class OAuth2 extends Resource
      * @param string $clientSecret The Client Secret of your app.
      * @param string $redirectURI The redirect URI that was used when the user authorized your app. This must exactly match the redirect_uri used when initiating the OAuth 2.0 connection.
      * @param string $tokenCode The code parameter returned to your redirect URI when the user authorized your app. Or a refresh token.
-     * @return \SevenShores\Hubspot\Http\Response
-     * @throws \SevenShores\Hubspot\Exceptions\BadRequest
+     * @return Response
+     * @throws BadRequest
      */
-    function getTokensByCode($clientId, $clientSecret, $redirectURI, $tokenCode)
+    public function getTokensByCode($clientId, $clientSecret, $redirectURI, $tokenCode)
     {
         $options['form_params'] = [
             'grant_type' => 'authorization_code',
@@ -44,7 +64,7 @@ class OAuth2 extends Resource
 
         $options['headers']['content-type'] = 'application/x-www-form-urlencoded';
 
-        return $this->client->request('post', $this->endpoint . '/token', $options);
+        return $this->client->request('post', $this->endpoint.'/token', $options);
     }
 
     /**
@@ -54,10 +74,10 @@ class OAuth2 extends Resource
      * @param string $clientId The Client ID of your app.
      * @param string $clientSecret The Client Secret of your app.
      * @param string $refreshToken The refresh token.
-     * @return \SevenShores\Hubspot\Http\Response
-     * @throws \SevenShores\Hubspot\Exceptions\BadRequest
+     * @return Response
+     * @throws BadRequest
      */
-    function getTokensByRefresh($clientId, $clientSecret, $refreshToken)
+    public function getTokensByRefresh($clientId, $clientSecret, $refreshToken)
     {
         $options['form_params'] = [
             'grant_type' => 'refresh_token',
@@ -68,43 +88,43 @@ class OAuth2 extends Resource
 
         $options['headers']['content-type'] = 'application/x-www-form-urlencoded';
 
-        return $this->client->request('post', $this->endpoint . '/token', $options);
+        return $this->client->request('post', $this->endpoint.'/token', $options);
     }
 
     /**
      * Get Information for OAuth 2.0 Access Token
      *
      * @param int $token The access token that you want to get the information for.
-     * @return \SevenShores\Hubspot\Http\Response
-     * @throws \SevenShores\Hubspot\Exceptions\BadRequest
+     * @return Response
+     * @throws BadRequest
      */
-    function getAccessTokenInfo($token)
+    public function getAccessTokenInfo($token)
     {
-        return $this->client->request('get', $this->endpoint . "/access-tokens/{$token}");
+        return $this->client->request('get', $this->endpoint."/access-tokens/{$token}");
     }
 
     /**
      * Get Information for OAuth 2.0 Refresh Token
      *
      * @param int $token The refresh token that you want to get the information for.
-     * @return \SevenShores\Hubspot\Http\Response
-     * @throws \SevenShores\Hubspot\Exceptions\BadRequest
+     * @return Response
+     * @throws BadRequest
      */
-    function getRefreshTokenInfo($token)
+    public function getRefreshTokenInfo($token)
     {
-        return $this->client->request('get', $this->endpoint . "/refresh-tokens/{$token}");
+        return $this->client->request('get', $this->endpoint."/refresh-tokens/{$token}");
     }
 
     /**
      * Delete OAuth 2.0 Refresh Token
      *
      * @param int $token The refresh token that you want to delete.
-     * @return \SevenShores\Hubspot\Http\Response
-     * @throws \SevenShores\Hubspot\Exceptions\BadRequest
+     * @return Response
+     * @throws BadRequest
      */
-    function deleteRefreshToken($token)
+    public function deleteRefreshToken($token)
     {
-        return $this->client->request('delete', $this->endpoint . "/refresh-tokens/{$token}");
+        return $this->client->request('delete', $this->endpoint."/refresh-tokens/{$token}");
     }
 
 }
