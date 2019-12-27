af013a718bde46f930e78e204b9307130abe14c9
ksvirkou <ksvirkou@hubspot.com>
2019-12-27 11:07:17 +0300

    review part 1

--- src/Resources/Workflows.php
+++ src/Resources/Workflows.php
@@ -2,82 +2,83 @@
 
 namespace SevenShores\Hubspot\Resources;
 
+/**
+ * @see https://developers.hubspot.com/docs/methods/workflows/v3/get_workflows
+ */
 class Workflows extends Resource
 {
     /**
      * Get all workflows.
+     * 
+     * @see https://developers.hubspot.com/docs/methods/workflows/v3/get_workflows
      *
      * @return \SevenShores\Hubspot\Http\Response
      */
     public function all()
     {
-        $endpoint = 'https://api.hubapi.com/automation/v2/workflows';
-
+        $endpoint = 'https://api.hubapi.com/automation/v3/workflows';
+        
         return $this->client->request('get', $endpoint);
     }
-
+    
     /**
      * Get a specific workflow.
      *
+     * @see https://developers.hubspot.com/docs/methods/workflows/v3/get_workflow
+     * 
      * @param int $id
      *
      * @return \SevenShores\Hubspot\Http\Response
      */
-    public function getById($id)
+    public function getById($id, array $params = [])
     {
-        $endpoint = "https://api.hubapi.com/automation/v2/workflows/{$id}";
-
-        return $this->client->request('get', $endpoint);
+        $endpoint = "https://api.hubapi.com/automation/v3/workflows/{$id}";
+        
+        return $this->client->request(
+            'get',
+            $endpoint,
+            [],
+            build_query_string($params)
+        );
     }
-
+    
     /**
      * Enroll a contact in a workflow.
      *
-     * @param int    $workflow_id
+     * @see https://developers.hubspot.com/docs/methods/workflows/add_contact
+     * 
+     * @param int    $workflowId
      * @param string $email
      *
      * @return \SevenShores\Hubspot\Http\Response
      */
-    public function enrollContact($workflow_id, $email)
+    public function enrollContact($workflowId, $email)
     {
-        $endpoint = "https://api.hubapi.com/automation/v2/workflows/{$workflow_id}/enrollments/contacts/{$email}";
-
+        $endpoint = "https://api.hubapi.com/automation/v2/workflows/{$workflowId}/enrollments/contacts/{$email}";
+        
         return $this->client->request('post', $endpoint);
     }
-
-    /**
-     * Unenroll a contact from a workflow.
-     *
-     * @param int    $workflow_id
-     * @param string $email
-     *
-     * @return \SevenShores\Hubspot\Http\Response
-     */
-    public function unenrollContact($workflow_id, $email)
-    {
-        $endpoint = "https://api.hubapi.com/automation/v2/workflows/{$workflow_id}/enrollments/contacts/{$email}";
-
-        return $this->client->request('delete', $endpoint);
-    }
-
+    
     /**
      * Create a new workflow.
      *
+     * @see https://developers.hubspot.com/docs/methods/workflows/v3/create_workflow
+     * 
      * @param array $workflow The workflow properties
      *
      * @return \SevenShores\Hubspot\Http\Response
      */
     public function create($workflow)
     {
-        $endpoint = 'https://api.hubapi.com/automation/v2/workflows';
-
+        $endpoint = 'https://api.hubapi.com/automation/v3/workflows';
         $options['json'] = $workflow;
-
         return $this->client->request('post', $endpoint, $options);
     }
-
+    
     /**
      * Delete a workflow.
+     * 
+     * @see https://developers.hubspot.com/docs/methods/workflows/v3/delete_workflow
      *
      * @param int $id
      *
@@ -85,60 +86,59 @@ class Workflows extends Resource
      */
     public function delete($id)
     {
-        $endpoint = "https://api.hubapi.com/automation/v2/workflows/{$id}";
-
-        $queryString = build_query_string(['updatedAt' => time()]);
-
-        return $this->client->request('delete', $endpoint, [], $queryString);
+        $endpoint = "https://api.hubapi.com/automation/v3/workflows/{$id}";
+        
+        return $this->client->request('delete', $endpoint, []);
     }
-
+    
+    /**
+     * Unenroll a contact from a workflow.
+     *
+     * @see https://developers.hubspot.com/docs/methods/workflows/remove_contact
+     * 
+     * @param int    $workflowId
+     * @param string $email
+     *
+     * @return \SevenShores\Hubspot\Http\Response
+     */
+    public function unenrollContact($workflowId, $email)
+    {
+        $endpoint = "https://api.hubapi.com/automation/v2/workflows/{$workflowId}/enrollments/contacts/{$email}";
+        
+        return $this->client->request('delete', $endpoint);
+    }
+    
     /**
      * Get current enrollments for a contact.
      *
-     * @param int $contact_id
+     * @param int $contactId
      *
      * @return \SevenShores\Hubspot\Http\Response
      */
-    public function enrollmentsForContact($contact_id)
+    public function enrollmentsForContact($contactId)
     {
-        $endpoint = "https://api.hubapi.com/automation/v2/workflows/enrollments/contacts/{$contact_id}";
-
+        $endpoint = "https://api.hubapi.com/automation/v2/workflows/enrollments/contacts/{$contactId}";
+        
         return $this->client->request('get', $endpoint);
     }
-
+    
     /**
-     * Get past events for contact from a workflow.
+     * Get a history of events for a specific workflow, filtered for a
+     * specific contact and/or event type(s).
      *
-     * @param int   $workflow_id
-     * @param int   $contact_id
-     * @param array $params      optional parameters
+     * @param int   $workflowId
+     * @param array $filter
+     * @param array $params      Optional parameters.
      *
      * @return \SevenShores\Hubspot\Http\Response
      */
-    public function pastEventsForContact($workflow_id, $contact_id, $params = [])
+    public function logEvents($workflowId, $filter, $params = [])
     {
-        $endpoint = " /automation/v2/workflows/{$workflow_id}/logevents/contacts/{$contact_id}/past";
-
+        $endpoint = "https://api.hubapi.com/automation/v3/logevents/workflows/{$workflowId}/filter";
+        
+        $options['json'] = $filter;
         $queryString = build_query_string($params);
-
-        return $this->client->request('get', $endpoint, [], $queryString);
-    }
-
-    /**
-     * Get upcoming (scheduled) events for a contact in a workflow.
-     *
-     * @param int   $workflow_id
-     * @param int   $contact_id
-     * @param array $params
-     *
-     * @return \SevenShores\Hubspot\Http\Response
-     */
-    public function upcomingEventsForContact($workflow_id, $contact_id, $params = [])
-    {
-        $endpoint = "https://api.hubapi.com/automation/v2/workflows/{$workflow_id}/logevents/contacts/{$contact_id}/upcoming";
-
-        $queryString = build_query_string($params);
-
-        return $this->client->request('get', $endpoint, [], $queryString);
+        
+        return $this->client->request('put', $endpoint, $options, $queryString);
     }
 }
