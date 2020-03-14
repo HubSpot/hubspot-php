98cf1c9c797f83d736108b9e681c91070dfcbf0a
ksvirkou <ksvirkou@hubspot.com>
2020-03-14 13:24:30 +0300

    update Crm Pipelines + tests

---
+++ tests/Integration/Resources/CrmPipelinesDealsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SevenShores\Hubspot\Tests\integration\Resources;
+
+use SevenShores\Hubspot\Tests\Integration\Abstraction\CrmPipelinesTestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class CrmPipelinesDealsTest extends CrmPipelinesTestCase
+{
+}
---
+++ tests/Integration/Resources/CrmPipelinesTicketsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SevenShores\Hubspot\Tests\integration\Resources;
+
+use SevenShores\Hubspot\Tests\Integration\Abstraction\CrmPipelinesTestCase;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class CrmPipelinesTicketsTest extends CrmPipelinesTestCase
+{
+    /**
+     * @var string
+     */
+    protected $type = 'tickets';
+}
